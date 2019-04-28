e04b611def95b06d7fdf7c60c41a52fd74d05432
Shizuka Hayashi <z5165356@ad.unsw.edu.au>
2019-04-28 18:28:58 +1000

    fixing bugs

--- ass3/src/agent.c
+++ ass3/src/agent.c
@@ -8,43 +8,62 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
-#include <stdbool.h>
+#include <assert.h>
 #include <math.h>
 #include "common.h"
 #include "agent.h"
 #include "game.h"
 
-#define MAX_MOVE 81
 
+#define true 1
+#define false 0
+
+struct node
+{
+    struct node *children;
+    int numOfCildren;
+    //node * parent;
+    int depth;
+    int prev_move;
+    int player;
+    int** boards;
+};
+
+int possible_num_ways_win(int player, struct node* state);
+int winState(int player);
+int isTerminal(struct node* state);
+double getHeuristic(struct node* state);
+int play();
+double min_value(struct node* state, double alpha, double beta);
+double max_value(struct node* state, double alpha, double beta);
+struct node** getChildren(struct node* game_state);
+int** copyBoard(int** boards);
+
+#define MAX_MOVE 81
+#define MAX_DEPTH 6
 int board[10][10];
 int move[MAX_MOVE+1];
 int player;
 int m;
 
-struct node
-{
-      node *children;
-      int numOfCildren;
-      node * parent;
-      int *d;
-}
-  
 /* newNode() allocates a new node with the given data(new_boards) with default 9 children*/
-struct node* newNode(node parent, int* new_boards) 
+struct node* newGameState(int** new_boards, int depth, int player, int prev_move)
 { 
-  // Allocate memory for new node  
-  struct node* node = (struct node*)malloc(sizeof(struct node)); 
-  node child_array[9];
-  // Assign data to this node 
-  node->data = new_boards; 
-  
-  node->numOfCildren = 9;
-  
-  node->children = child_array;// array of children
+    // Allocate memory for new node
+    struct node* node = (struct node*)malloc(sizeof(struct node));
+    //node child_array[9];
+    // Assign data to this node
+    node->boards = new_boards;
+    node->depth = depth;
+    node->player = player;
+    node->prev_move = prev_move;
+    //node->numOfCildren = 9;
 
-  node->parent = parent;
-  
-
+    //node->children = child_array;
+    // array of children
+    //node->parent = parent;
+    
+    
   return(node); 
 } 
 /*********************************************************//*
@@ -118,9 +137,7 @@ int agent_second_move( int board_num, int prev_move )
   move[1] = prev_move;
   board[board_num][prev_move] = !player;
   m = 2;
-  do {
-    this_move = 1 + random()% 9;
-  } while( board[prev_move][this_move] != EMPTY );
+  this_move = play();
   move[m] = this_move;
   board[prev_move][this_move] = player;
   return( this_move );
@@ -142,9 +159,7 @@ int agent_third_move(
   board[board_num][first_move] =  player;
   board[first_move][prev_move] = !player;
   m=3;
-  do {
-    this_move = 1 + random()% 9;
-  } while( board[prev_move][this_move] != EMPTY );
+  this_move = play();
   move[m] = this_move;
   board[move[m-1]][this_move] = player;
   return( this_move );
@@ -160,10 +175,7 @@ int agent_next_move( int prev_move )
   move[m] = prev_move;
   board[move[m-1]][move[m]] = !player;
   m++;
-  do {
-    //TODO
-    this_move = 1 + random()% 9;
-  } while( board[prev_move][this_move] != EMPTY );
+ this_move = play();
   move[m] = this_move;
   board[move[m-1]][this_move] = player;
   return( this_move );
@@ -201,150 +213,237 @@ void agent_cleanup()
 
 /*********************************************************//*
 */
-bool winState(int player)
+int winState(int player)
 {
-  int curr_boards = board;
-  for i in range(1,10):
-      if ((player==curr_boards[i][1]&& player ==curr_boards[i][2]&& player==curr_boards[i][3]) ||
-      (player==curr_boards[i][4]&& player==curr_boards[i][5]&& player==curr_boards[i][6])||
-      (player==curr_boards[i][7]&& player==curr_boards[i][8]&& player==curr_boards[i][9])||
-      (player==curr_boards[i][1]&& player==curr_boards[i][4]&& player==curr_boards[i][7])||
-      (player==curr_boards[i][2]&& player==curr_boards[i][5]&& player==curr_boards[i][8])||
-      (player==curr_boards[i][3]&& player==curr_boards[i][6]&& player==curr_boards[i][9])||
-      (player==curr_boards[i][1]&& player==curr_boards[i][5]&& player==curr_boards[i][9])||
-      (player==curr_boards[i][3]&& player==curr_boards[i][5]&& player==curr_boards[i][7])
+  for (int i = 1 ;i < 10;i++){
+      if ((player==board[i][1]&& player ==board[i][2]&& player==board[i][3]) ||
+      (player==board[i][4]&& player==board[i][5]&& player==board[i][6])||
+      (player==board[i][7]&& player==board[i][8]&& player==board[i][9])||
+      (player==board[i][1]&& player==board[i][4]&& player==board[i][7])||
+      (player==board[i][2]&& player==board[i][5]&& player==board[i][8])||
+      (player==board[i][3]&& player==board[i][6]&& player==board[i][9])||
+      (player==board[i][1]&& player==board[i][5]&& player==board[i][9])||
+      (player==board[i][3]&& player==board[i][5]&& player==board[i][7]))
       return true;
+  }
   return false ;
 }                                                          
                                                                                       
 /*********************************************************//*
 */
-int possible_num_ways_win(int player)
+int possible_num_ways_win(int player, struct node* state)
 {
-  curr_boards = boards
-  num = 0
-  if(player == 1):
-      player2 =2
-  else:
-      player2 =1 
+  int** curr_boards = state->boards;
+  int num = 0;
+  int player2 = 0;
+  if(player == 1)
+      player2 =2;
+  else
+      player2 =1;
 
-  for i in range(1,10){
+  for (int i=1; i<10; i++){
       if (curr_boards[i][1]==player || curr_boards[i][2]==player || curr_boards[i][3]==player){
-          if not (curr_boards[i][1]==player2 || curr_boards[i][2]==player2 || curr_boards[i][3]==player2)
+          if (!(curr_boards[i][1]==player2 || curr_boards[i][2]==player2 || curr_boards[i][3]==player2))
               num = num+1;
       }
       if(curr_boards[i][4]==player || curr_boards[i][5]==player || curr_boards[i][6] == player){
-          if not (curr_boards[i][4]==player2 || curr_boards[i][5]==player2 || curr_boards[i][6] == player2)
+          if (! (curr_boards[i][4]==player2 || curr_boards[i][5]==player2 || curr_boards[i][6] == player2))
               num = num+1;
       }
       if(curr_boards[i][7]==player || curr_boards[i][8]==player || curr_boards[i][9] == player){
-          if not (curr_boards[i][7]==player2 || curr_boards[i][8]==player2 || curr_boards[i][9] == player2)
+          if (! (curr_boards[i][7]==player2 || curr_boards[i][8]==player2 || curr_boards[i][9] == player2))
               num = num+1;
       }
       if(curr_boards[i][1]==player || curr_boards[i][4]==player || curr_boards[i][7] == player){
-          if not (curr_boards[i][1]==player2 || curr_boards[i][4]==player2 || curr_boards[i][7] == player2)
+          if (! (curr_boards[i][1]==player2 || curr_boards[i][4]==player2 || curr_boards[i][7] == player2))
               num = num+1;
       }
       if(curr_boards[i][2]==player || curr_boards[i][5]==player || curr_boards[i][8] == player){
-          if not (curr_boards[i][2]==player2 || curr_boards[i][5]==player2 || curr_boards[i][8] == player2)
+          if (! (curr_boards[i][2]==player2 || curr_boards[i][5]==player2 || curr_boards[i][8] == player2))
               num = num+1;
       }
       if(curr_boards[i][3]==player || curr_boards[i][6]==player || curr_boards[i][9] == player){
-          if not (curr_boards[i][3]==player2 || curr_boards[i][6]==player2 || curr_boards[i][9] == player2)
+          if (! (curr_boards[i][3]==player2 || curr_boards[i][6]==player2 || curr_boards[i][9] == player2))
               num = num+1;
       }
       if(curr_boards[i][1]==player || curr_boards[i][5]==player || curr_boards[i][9] ==player){
-          if not (curr_boards[i][1]==player2 || curr_boards[i][5]==player2 || curr_boards[i][9] == player2)
+          if (! (curr_boards[i][1]==player2 || curr_boards[i][5]==player2 || curr_boards[i][9] == player2))
               num = num+1;
       }
       if(curr_boards[i][3]==player || curr_boards[i][5]==player || curr_boards[i][7] == player){
-          if not (curr_boards[i][3]==player2 || curr_boards[i][5]==player2 || curr_boards[i][7] == player2)
+          if (! (curr_boards[i][3]==player2 || curr_boards[i][5]==player2 || curr_boards[i][7] == player2))
               num = num+1;
       }
   }
   return num;
-}                                                     
-/*********************************************************//*
-peseudo code
+}        
+//TODO
+int isTerminal(struct node* state){
 
-function alphabeta(node, depth, α, β, maximizingPlayer) is
-    if depth = 0 or node is a terminal node then
-        return the heuristic value of node
-    if maximizingPlayer then
-        value := −∞
-        for each child of node do
-            value := max(value, alphabeta(child, depth − 1, α, β, FALSE))
-            α := max(α, value)
-            if α ≥ β then
-                break (* β cut-off *)
-        return value
-    else
-        value := +∞
-        for each child of node do
-            value := min(value, alphabeta(child, depth − 1, α, β, TRUE))
-            β := min(β, value)
-            if α ≥ β then
-                break (* α cut-off *)
-        return value
-*/
-// TODO 
-int alphabeta(node curr_board, int depth, double alpha, double beta, bool maximising)
-{
-  if (depth = 0 || terminalNode(curr_board))
-    return getHeuristic();
-  
-  if (maximising){
-    int value =  -INFINITY;
-    getChild(curr_board);
-
-    for(int i=0; i++; curr_board->children[i]!=NULL){
-
-      value = max(value, alphabeta(child, depth-1, alpha, beta, false));
-      alpha = max(alpha, value);
-      if(alpha >= beta){
-        break;
-      }
-    }
-    return value
-  }
-  else{
-    int value = INFINITY;
-    for(int i=0; i++; curr_board->children[i]!=NULL){
-      value = min(value, alphabeta(child, depth-1, alpha, beta, true));
-      beta = min(beta, value);
-      if(alpha>=beta){
-        break;
-      }
-    }
-    return value
-  }
-
-bool terminalNode(node curr_board){
-  if(getHeuristic(curr_board)== INFINITY || getHeuristic(curr_board)== -INFINITY)
+  if(state->depth ==MAX_DEPTH)
     return true;
+
+  return false;
+}
+
 // TODO 
-double getHeuristic(node curr_board){
+double getHeuristic(struct node* state){
+    return possible_num_ways_win(1, state) - possible_num_ways_win(2, state);
+}
+
+struct node * alphabeta(struct node* game_state){
+    int best_val = -INFINITY;
+    double beta = INFINITY;
+    
+    //MAYBE FAULSE
+    //TODO
+    struct node** children = getChildren(game_state);
+    struct node* best_state = NULL;
+    for(int i=1; i<10; i++){
+        struct node* state = children[i];
+        if(state==NULL) continue;
+        
+        int value = min_value(state, best_val, beta);
+        if(value > best_val){
+            best_val = value;
+            best_state = state;
+        }
+    }
+    return best_state;
+}
+int max(double A, double B){
+  return A>B;
+}
+
+int min(double A, double B){
+  return A<B;
+}
+double max_value(struct node* state, double alpha, double beta){
+  if(isTerminal(state))
+    return getHeuristic(state);
+
+  double value = -INFINITY;
+  double eval=0;
+
+  if(getHeuristic(state)==INFINITY)
+    return INFINITY;
+  if (getHeuristic(state)==-INFINITY)
+    return -INFINITY;
+
+  struct node** children = getChildren(state);
   
-}
-// TODO 
-void getChild(node curr_board){
-  int temp_board[][] = copyBoard(curr_board.data);
-
-  //TODO double check if move[m]==prev_move
-  for(int i=0; i<=10; i++){
-    temp_board[move[m]][i] = 
+  for(int i=1; i<10; i++){
+    struct node* state = children[i];
+    if(state==NULL)
+      continue;
+    
+    
+    eval = min_value(state, alpha, beta);
+    value = max(value, eval);
+    alpha = max(alpha, eval);
+    if(beta <= alpha)
+      break;
   }
-
+  return value;
 }
 
-int** copyBoard(int board[][]){
-  int new_board[10][10];
+double min_value(struct node* state, double alpha, double beta){
+  if(isTerminal(state))
+    return getHeuristic(state);
 
-  for(int i=0; i<=10; i++){
-    for(int j=0; j<=10; j++){
-      new_baord[i][j] = board[i][j];
+  double value = INFINITY;
+  double eval = 0;
+  
+  if(getHeuristic(state)==INFINITY)
+    return INFINITY;
+  if(getHeuristic(state)==-INFINITY)
+    return -INFINITY;
+
+  struct node** children = getChildren(state);
+
+  for(int i=1; i<10; i++){
+    struct node* state = children[i];
+    if(state==NULL)
+      continue;
+    eval = max_value(state, alpha, beta);
+    value = min(value, eval);
+    alpha = min(alpha, eval);
+    if(beta <= alpha)
+      break;
+  }
+  return value;
+}
+
+
+int play(){
+  int next_move = 0;
+  //TODO
+  int curr = move[m];
+  struct node* game_state = newGameState(board, 0, 2, curr);
+  struct node* nextState = alphabeta(game_state);
+  if(nextState == NULL){
+    printf("I LOSE");
+    for(int i=1; i<10; i++){
+      if(board[m][i]==0)
+        return i;
     }
   }
-  return new_board;
+
+  int** next_board = nextState->boards;
+  
+  for(int i=1; i<10; i++){
+    for(int j=1; j<10; j++){
+      if(board[i][j]!=next_board[i][j]){
+        next_move = j;
+        break;
+      }
+
+    }
+  }
+
+  return next_move;
 }
+
+struct node** getChildren(struct node* game_state){
+    int curr_depth = game_state->depth;
+    int next_depth = curr_depth + 1;
+    int next_player = 0;
+    if(game_state->player ==1)
+        next_player = 2;
+    else
+        next_player=1;
+
+    struct node* children[10];
+    for(int i=1; i<10; i++)
+        children[i] = malloc(sizeof(struct node));
+    
+    int board = game_state->prev_move;
+    int** boards = game_state->boards;
+    for(int i=1; i<10; i++){
+        if(boards[board][i]==0){
+            
+            int **next_boards = copyBoard(boards);
+            next_boards[board][i] = next_player;
+            struct node* next_game_state = newGameState(next_boards, next_depth, next_player, i);
+            children[i] = next_game_state;
+            assert(boards!=NULL);
+        }
+    }
+
+    return children;
+
+}
+    
+int** copyBoard(int** boards){
+  int** new_boards = malloc(100*sizeof(int));
+
+  for(int i=1; i<10; i++){
+    for(int j=1; j<10; j++){
+      new_boards[i][j] = boards[i][j];
+    }
+  }
+  return new_boards;
+}
+
+
   
